6318746c303d2b18dcdf3b066a91188d7610002a
Thierry Pelais de Oliveira <compasso-thierry.oliveira@ailos.coop.br>
2022-09-01 00:16:02 -0300

    feat: add base project

---
+++ lib/core/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blue = Color(0xff5265F5);
+  static const Color black = Color(0xff191924);
+  static const Color grey = Color(0xff283139);
+  static const Color red = Color(0xffF2545B);
+  static const Color green = Color(0xff04A777);
+}
